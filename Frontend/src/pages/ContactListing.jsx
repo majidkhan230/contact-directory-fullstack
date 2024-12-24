80eac5298217aecf2e3c3ab9988ad1b63eca8224
MajidKh230 <khanmajid230@gmail.com>
2024-12-25 01:34:17 +0500

    add search

--- Frontend/src/pages/ContactListing.jsx
+++ Frontend/src/pages/ContactListing.jsx
@@ -1,16 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { delReq, getReq, updReq } from "../../api/axios";
 import { useDispatch, useSelector } from "react-redux";
-import { delContact, setContacts } from "../store/features/contactReducer";
+import { delContact,  setContacts } from "../store/features/contactReducer";
 import { useNavigate } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
-import { MdOutlineEdit,MdDelete } from "react-icons/md";
-import dummyImg from '/assets/images/avatar.gif'
+import { MdOutlineEdit, MdDelete } from "react-icons/md";
+import dummyImg from "/assets/images/avatar.gif";
 function ContactListing() {
+  const [searchTerm,setSearchTerm ] = useState('')
+  
   const navigate = useNavigate();
-
+  
   const { contacts } = useSelector((state) => state.contacts);
   console.log({ contacts });
+
+  // const [filterData,setFilterData ] = useState(contacts)
+
+  const filteredContacts = contacts.filter((item) =>
+    Object.values(item).some((value) =>
+      value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  );
 
   const dispatch = useDispatch();
 
@@ -37,6 +47,19 @@ function ContactListing() {
     dispatch(delContact(id));
   };
 
+  //onSearch
+
+  // const onSearch = (e) => {
+  //   e.preventDefault()
+  //   setSearchTerm(e.target.value)
+  // const filtered =  contacts.filter((item)=>{
+  //     return Object.values(item).some(value=>value.toString().toLowerCase().includes(searchTerm.toLowerCase()))
+  //   })
+  //   setFilterData(filtered)
+  // };
+
+  
+
   return (
     <div className="w-full bg-[#F5F5FA] p-6">
       <div className="container mx-auto">
@@ -48,13 +71,17 @@ function ContactListing() {
             type="text"
             className="w-full outline-none"
             placeholder="Name, email or phone number"
+            value = {searchTerm}
+            // onChange={(e)=>onSearch(e)}
+             onChange={(e)=>setSearchTerm(e.target.value)}
+            
           />
           <CiSearch className="w-6 h-6 text-gray-500" />
         </div>
       </div>
 
       <div className="contacts mt-6">
-        {contacts.map((contact, index) => (
+        {filteredContacts?.map((contact, index) => (
           <div
             key={index}
             className="flex justify-between items-center px-4 py-2 bg-white rounded-lg shadow-sm mb-2"
@@ -71,7 +98,6 @@ function ContactListing() {
               </div>
             </div>
             <div className="flex space-x-2">
-             
               <button
                 className="bg-blue-500 text-white p-2 rounded-full hover:bg-blue-600 mr-2"
                 onClick={() => onEdit(contact?._id)}
@@ -82,7 +108,7 @@ function ContactListing() {
                 className="bg-red-500 text-white  p-2 rounded-full hover:bg-red-600"
                 onClick={() => onDelete(contact?._id)}
               >
-               <MdDelete className="w-5 h-5" />
+                <MdDelete className="w-5 h-5" />
               </button>
             </div>
           </div>
@@ -92,4 +118,4 @@ function ContactListing() {
   );
 }
 
-export default ContactListing;
+export default ContactListing
--- Frontend/src/store/features/contactReducer.js
+++ Frontend/src/store/features/contactReducer.js
@@ -1,37 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    contacts:[]
-}
+  contacts: [],
+};
 
+export const contactSlice = createSlice({
+  name: "contact",
+  initialState,
+  reducers: {
+    setContacts: (state, { payload }) => {
+      state.contacts = payload;
+    },
+    addContact: (state, { payload }) => {
+      state.contacts.push(payload);
+    },
+    updContact: (state, { payload }) => {
+      const index = state.contacts.findIndex((item) => item._id == payload);
+      if (index !== -1) {
+        state.contacts[index] = payload;
+      }
+    },
+    delContact: (state, { payload }) => {
+      state.contacts = state.contacts.filter((item) => item._id !== payload);
+    },
+    // filterContact: (state, { payload }) => {
+    //     state.contacts = state.contacts.filter((item) =>
+    //       Object.values(item).some((value) =>
+    //         value.toString().toLowerCase().includes(payload.toLowerCase())
+    //       )
+    //     );
+    //   },
+      
+  },
+});
 
-export const contactSlice = createSlice(
-    {
-        name:"contact",
-        initialState,
-        reducers:{
-            setContacts:(state,{payload})=>{
-             state.contacts = payload
-            },
-            addContact:(state,{payload})=>{
-                 state.contacts.push(payload)
-            },
-            updContact:(state,{payload})=>{
-                const index = state.contacts.findIndex((item)=>(item._id == payload ))
-                if(index !== -1){
-                     state.contacts[index] = payload
-                }
-            },
-            delContact:(state,{payload})=>{
-                 state.contacts =  state.contacts.filter((item)=>(item._id !==payload))
-            }
-        }
-    }
-)
+export const {
+  setContacts,
+  addContact,
+  updContact,
+  delContact,
+} = contactSlice.actions;
 
-
-export const {setContacts,addContact,updContact,delContact} = contactSlice.actions
-
-
-export default contactSlice.reducer
-
+export default contactSlice.reducer;
