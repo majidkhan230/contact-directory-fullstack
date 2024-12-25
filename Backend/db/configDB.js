5a44e34d596117f89709badab030db4bf05716af
MajidKh230 <khanmajid230@gmail.com>
2024-12-25 15:34:01 +0500

    modify configDB

--- Backend/db/configDB.js
+++ Backend/db/configDB.js
@@ -1,25 +1,16 @@
+// db/configDB.js
 import mongoose from "mongoose";
 
-let isConnected = false; // Track the connection state
-
 async function connectDB() {
-  if (isConnected) {
-    console.log('Using existing database connection');
-    return;
-  }
-  try {
-    const db = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    isConnected = db.connections[0].readyState;
-    console.log('Database connected successfully');
-  } catch (error) {
-    console.error({
-      message: "Failed to connect to DB",
-      error: error.message,
-    });
-  }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI); // Options are no longer needed
+        console.log('DB is successfully connected');
+    } catch (error) {
+        console.error({
+            message: "Failed to connect to DB",
+            error: error.message
+        });
+    }
 }
 
 export default connectDB;
