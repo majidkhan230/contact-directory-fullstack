73f3e4985d0b45747fe37fa378b2b0df0db249bb
MajidKh230 <khanmajid230@gmail.com>
2024-12-25 15:30:05 +0500

    update vercel.json and configDB

--- Backend/db/configDB.js
+++ Backend/db/configDB.js
@@ -1,16 +1,25 @@
 import mongoose from "mongoose";
 
-async function connectDB(){
-try {
-    await mongoose.connect(process.env.MONGODB_URI)
-    console.log('db is sucessfully connected')
-} catch (error) {
-    console.error({
-        message:"failed to connect to DB",
-        error:error.message
-    })
-}    
-}
+let isConnected = false; // Track the connection state
 
+async function connectDB() {
+  if (isConnected) {
+    console.log('Using existing database connection');
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = db.connections[0].readyState;
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error({
+      message: "Failed to connect to DB",
+      error: error.message,
+    });
+  }
+}
 
 export default connectDB;
