28296399e449eceb63e119d6870f5f1c8a92ea78
MajidKh230 <khanmajid230@gmail.com>
2024-12-26 23:16:02 +0500

    add some changes

--- Frontend/api/axios.js
+++ Frontend/api/axios.js
@@ -1,43 +1,39 @@
 import apiClient from "./index";
 
 const getReq = async (path) => {
-try {
+  try {
     const res = await apiClient.get(path);
     return res;
-} catch (error) {
-    console.log(error)
-}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-
-
-const postReq = async (path,data) => {
-try {
-    const res = await apiClient.post(path,data);
+const postReq = async (path, data) => {
+  try {
+    const res = await apiClient.post(path, data);
     return res;
-} catch (error) {
-    console.log(error)
-}
-  };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const updReq = async (path,data) => {
-try {
-    const res = await apiClient.put(path,data);
+const updReq = async (path, data) => {
+  try {
+    const res = await apiClient.put(path, data);
     return res;
-} catch (error) {
-    console.log(error)
-}
-  };
-
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const delReq = async (path) => {
-try {
+  try {
     const res = await apiClient.delete(path);
     return res;
-} catch (error) {
-    console.log(error)
-}
-  };
-  
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  export  {getReq,postReq,updReq,delReq}
+export { getReq, postReq, updReq, delReq };
