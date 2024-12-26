f05a9b23c914f56ec4bf49e7367dc6932421ed0a
MajidKh230 <khanmajid230@gmail.com>
2024-12-26 15:46:10 +0500

    fix some changes for serverless connection

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import contactRoutes from './routes/contactRoutes.js'
-import connectDB from './db/configDB.js'
+import mongoose from 'mongoose'
+// import connectDB from './db/configDB.js'
 
 
 const app = express()
@@ -23,7 +24,16 @@ const PORT =process.env.PORT || 8080
 
 app.listen(PORT,()=>{
     console.log(`server is sucessfully running on ${PORT}`)
-    connectDB()
+    // connectDB()
+})
+
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log("DB connection is successfully established!")
+})
+.catch((error)=>{
+    console.error("failed to connect to DB",error.message)
 })
 
 
